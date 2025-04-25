d6299e92940519353c94bfdd68a5727c4874ff8b
sayedhany <sayedh@mail.ru>
2025-04-25 16:03:00 +0300

    add sidebar

--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -1,18 +1,19 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { SidebarModule } from 'primeng/sidebar';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent, MatSidenavModule],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, SidebarModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
+  sidebarVisible =signal(false);
 
 }
